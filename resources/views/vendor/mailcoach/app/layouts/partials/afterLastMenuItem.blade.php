a7e2e6d2c15df6b27bd83359a336dca47488a4fe
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2019-11-19 18:23:01 +0100

    Move service menu to dropdown

--- resources/views/vendor/mailcoach/app/layouts/partials/afterLastMenuItem.blade.php
+++ resources/views/vendor/mailcoach/app/layouts/partials/afterLastMenuItem.blade.php
@@ -1,14 +1,22 @@
-<x-navigation-item :href="route('account')">
-    Account
-</x-navigation-item>
-
-<x-navigation-item :href="route('users')">
-    Users
-</x-navigation-item>
-
-
-<li>
-    <form method="post" action="{{ route('logout') }}">
-        <button type="submit">Log out</button>
-    </form>
+<li class="dropdown" data-dropdown>
+    <button data-dropdown-trigger>
+        <img class="w-4 h-4 bg-blue-300 rounded-full">
+    </button>
+    <ul class="dropdown-list | hidden" data-dropdown-list>
+        <li>
+            <a href="{{ route('account') }}">
+                Account
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('users') }}">
+                Users
+            </a>
+        </li>
+        <li>
+            <form method="post" action="{{ route('logout') }}">
+                <button type="submit">Log out</button>
+            </form>
+        </li>
+    </ul>
 </li>
