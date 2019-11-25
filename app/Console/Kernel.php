2cc79838bcecacc9b19a4a99d2777ea2dd151e18
freek <freek@spatie.be>
2019-11-25 16:20:55 +0100

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,10 +4,15 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Laravel\Horizon\Console\TerminateCommand;
 use Spatie\Mailcoach\Commands\CalculateStatisticsCommand;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        TerminateCommand::class,
+    ];
+
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(CalculateStatisticsCommand::class)->everyMinute();
--- resources/views/app/layouts/partials/health.blade.php
+++ resources/views/app/layouts/partials/health.blade.php
@@ -1,14 +1,15 @@
-@if(! $mailConfigurationValid)
-    @if (! request()->routeIs('mailConfiguration'))
+@if (! request()->routeIs('mailConfiguration'))
+    @if(! $mailConfigurationValid)
         <div style="z-index: 10000" class="bg-red-200">
             Your mail configuration is invalid. Head over to the <a href="{{ route('mailConfiguration') }}">mail
                 configuration</a> screen.
         </div>
     @endif
-@endif
 
-@if(! $horizonActive)
-    <div style="z-index: 10000" class="bg-red-200">
-        Horizon is not active
-    </div>
+    @if(! $horizonActive)
+        <div style="z-index: 10000" class="bg-red-200">
+            Horizon is not active
+        </div>
+    @endif
+
 @endif
