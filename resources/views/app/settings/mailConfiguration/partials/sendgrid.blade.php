c8bfa2f313c50779196d747bf66af729b3629bf2
freek <freek@spatie.be>
2019-11-27 17:34:14 +0100

    wip

--- resources/views/app/settings/mailConfiguration/partials/sendgrid.blade.php
+++ resources/views/app/settings/mailConfiguration/partials/sendgrid.blade.php
@@ -1,11 +1,6 @@
 TODO: add link to docs
 
-<x-text-field label="Host" name="sendgrid_host" type="text" :value="$mailConfiguration->sendgrid_host"/>
-<x-text-field label="Port" name="sendgrid_port" type="text" :value="$mailConfiguration->sendgrid_port"/>
-<x-text-field label="Username" name="sendgrid_username" type="text"
-                :value="$mailConfiguration->sendgrid_username"/>
-<x-text-field label="Password" name="sendgrid_password" type="password"
-                :value="$mailConfiguration->sendgrid_password"/>
+<x-text-field label="API key" name="sendgrid_api_key" type="text" :value="$mailConfiguration->sendgrid_api_key"/>
 <x-text-field label="Webhook signing secret" name="sendgrid_signing_secret" type="secret"
               :value="$mailConfiguration->sendgrid_signing_secret"/>
 
