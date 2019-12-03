7414cf7b36c916741f57c6724829400e672ddd5a
Willem Van Bockstal <willem@spatie.be>
2019-12-03 16:41:21 +0100

    Health screen

--- resources/views/app/layouts/partials/health.blade.php
+++ resources/views/app/layouts/partials/health.blade.php
@@ -1,15 +1,23 @@
-@if (! request()->routeIs('mailConfiguration'))
-    @if(! $mailConfigurationValid)
-        <div class="z-50 p-1 text-center text-xs text-red-200 bg-red-500">
-            Your mail configuration is invalid. Head over to the <a href="{{ route('mailConfiguration') }}">mail
-                configuration</a> screen.
-        </div>
-    @endif
+@if (! request()->routeIs('mailConfiguration') || ! $horizonActive)
+    <div class="z-50 text-red-100 bg-red-500 px-4 py-4">
+        <div class="max-w-6xl mx-auto grid gap-1">
+            @if (! request()->routeIs('mailConfiguration'))
+                @if(! $mailConfigurationValid)
+                    <div class="flex items-baseline">
+                        <span class="w-6"><i class="fas fa-cog opacity-50"></i></span>
+                        <span class="ml-2">Your <strong>mail configuration</strong> is invalid. Head over to the <a class="underline" href="{{ route('mailConfiguration') }}">mail
+                            configuration</a> screen.</span>
+                    </div>
+                @endif
 
-    @if(! $horizonActive)
-        <div class="z-50 p-1 text-center text-xs text-red-200 bg-red-500">
-            Horizon is not active.
-        </div>
-    @endif
+                @if(! $horizonActive)
+                    <div class="flex items-baseline">
+                        <span class="w-6"><i class="fas fa-database opacity-50"></i></span>
+                        <span class="ml-2"><strong>Horizon</strong> is not active on your server.</span>
+                    </div>
+                @endif
 
+            @endif
+        </div>
+    </div>
 @endif
