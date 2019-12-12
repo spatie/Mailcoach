efb694d6b3966685b7ae6d7de499bac3ad273497
freek <freek@spatie.be>
2019-12-12 16:59:12 +0100

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>MailCoach</title>
+        <title>Mailcoach</title>
 
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
