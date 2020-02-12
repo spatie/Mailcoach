590d8ebb389439aca9f192877397c061ed0f6158
Willem Van Bockstal <willem@spatie.be>
2020-02-12 12:12:17 +0100

    Update favicon

--- resources/views/auth/layouts/master.blade.php
+++ resources/views/auth/layouts/master.blade.php
@@ -14,7 +14,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">
-    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileColor" content="#1288ff">
     <meta name="theme-color" content="#ffffff">
 
     <!-- CSRF Token -->
