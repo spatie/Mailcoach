cf107d07946916e4cee63e28fc583fb5626a5a12
freek <freek@spatie.be>
2020-02-19 10:36:24 +0100

    fix scheduled command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,6 +7,7 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Laravel\Horizon\Console\TerminateCommand;
 use Spatie\Mailcoach\Commands\CalculateStatisticsCommand;
+use Spatie\Mailcoach\Commands\SendScheduledCampaignsCommand;
 
 class Kernel extends ConsoleKernel
 {
@@ -18,7 +19,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(CalculateStatisticsCommand::class)->everyMinute();
-        $schedule->command('email-campaigns:send-scheduled-campaigns')->everyMinute();
+        $schedule->command(SendScheduledCampaignsCommand::class)->everyMinute();
         $schedule->command('clean:models')->daily();
     }
 }
