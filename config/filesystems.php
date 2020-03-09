472310afa1f40cdf69d23cfec9bf92ccb768ea33
freek <freek@spatie.be>
2020-03-09 21:05:49 +0100

    v2

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,6 +48,11 @@ return [
             'root' => storage_path('app'),
         ],
 
+        'media' => [
+            'driver' => 'local',
+            'root' => public_path('media'),
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
