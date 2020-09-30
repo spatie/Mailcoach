202432f6cf78ddba5dd37b9ba515419eb79f2dcf
Rias <hey@rias.be>
2020-09-30 09:55:25 +0200

    Move mailcoachUi route registration to provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,6 +37,8 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->configureRateLimiting();
 
+        Route::mailcoachUi('/');
+
         $this->routes(function () {
             Route::prefix('api')
                 ->middleware('api')
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,2 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::mailcoachUi('/');
