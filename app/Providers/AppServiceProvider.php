d078f10cf203b9fc375be268cf2ecfe9771ac572
Niels Vanpachtenbeke <10651054+Nielsvanpach@users.noreply.github.com>
2023-09-19 15:20:13 +0200

    register command

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Console\MakeUserCommand;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->commands([
+            MakeUserCommand::class,
+        ]);
     }
 
     /**
