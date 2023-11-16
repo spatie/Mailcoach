16bc1cae94d9b3da0e9056d5dff764a3be62a95f
Antoni Siek <antoni@siek.io>
2023-11-16 13:49:39 +0100

    fix: WelcomesNewUsers

--- app/Http/Middleware/WelcomesNewUsers.php
+++ app/Http/Middleware/WelcomesNewUsers.php
@@ -14,15 +14,15 @@ class WelcomesNewUsers
             abort(Response::HTTP_FORBIDDEN, __mc('The welcome link does not have a valid signature or is expired.'));
         }
 
-        if (! $request->mailcoachUser) {
+        if (! $request->user) {
             abort(Response::HTTP_FORBIDDEN, __mc('Could not find a user to be welcomed.'));
         }
 
-        if (is_null($request->mailcoachUser->welcome_valid_until)) {
+        if (is_null($request->user->welcome_valid_until)) {
             abort(Response::HTTP_FORBIDDEN, __mc('The welcome link has already been used.'));
         }
 
-        if (Carbon::create($request->mailcoachUser->welcome_valid_until)->isPast()) {
+        if (Carbon::create($request->user->welcome_valid_until)->isPast()) {
             abort(Response::HTTP_FORBIDDEN, __mc('The welcome link has expired.'));
         }
 
