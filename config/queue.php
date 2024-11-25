bb0413451d60d71eeae4419aead6efba3a0cb329
Rias <hey@rias.be>
2024-11-25 14:31:14 +0100

    Update migrations

--- config/queue.php
+++ config/queue.php
@@ -72,15 +72,6 @@ return [
             'after_commit' => false,
         ],
 
-        'mailcoach-redis' => [
-            'driver' => 'redis',
-            'connection' => env('REDIS_QUEUE_CONNECTION', 'default'),
-            'queue' => env('REDIS_QUEUE', 'default'),
-            'retry_after' => (int) env('REDIS_QUEUE_RETRY_AFTER', 60 * 60),
-            'block_for' => null,
-            'after_commit' => false,
-        ],
-
     ],
 
     /*
