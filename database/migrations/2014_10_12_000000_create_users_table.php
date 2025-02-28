d5afdf4833e755717dbfa530c9c53af3eca1f977
Rias <hey@rias.be>
2025-02-28 09:44:05 +0100

    Add welcome notification to starter project

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('welcome_valid_until')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
